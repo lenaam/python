ee94f922f8460df54f789454e8d0b4ae69ac6d43
lenaam <37027534+lenaam@users.noreply.github.com>
2019-08-24 02:24:58 +0300

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,19 @@
+#this a comment
+print("hello world")
+
+print("hello world!") #this is a comment
+
+#print("hello world")
+print ("cheers , mate!")
+
+#this is a comment
+#written in
+#more than one line
+print("hello , wolrd")
+""""
+this is a comment 
+written in
+morethan  one line
+"""
+
+print ("hello world!!")
---
+++ day3.py
@@ -0,0 +1,18 @@
+x , y , z = "orange" , "banana" , "cherry"
+
+print (x)
+print (y)
+print (z)
+print ("*********************")
+x1 = " awesome"
+print ("paython is "+x1)
+print ("**********************")
+x2 = "paython is"
+y2= " awesome"
+z2 = x2 + y2
+print (z2)
+print("***********************")
+x3 = 5
+y3 = 10
+print (x3 +y3)
+
